8b76b129917d0783fdaabc1fc5f689e3661f0e47
Ophelia Liu <ophelialiuyj@gmail.com>
2020-03-01 01:43:01 -0500

    week 6 assignment

---
+++ note.js
@@ -0,0 +1,8 @@
+elem = $('#num1')
+elem.val()
+elem.val(777)  //-> filter box now has 777 as input
+
+<br?
+>button (id = "my-button")Plot Area </button>
+
+elem.click(function(e) {alert('success')})
