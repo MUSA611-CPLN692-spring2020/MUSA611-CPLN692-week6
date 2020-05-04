bbf5f97c2d1c436cf09e0ed77d6eee9c79ab4c51
Leo <48158850+leonardoharth@users.noreply.github.com>
2020-05-04 12:12:52 -0400

    Update main.js

--- assignment/js/main.js
+++ assignment/js/main.js
@@ -13,11 +13,71 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   ext: 'png'
 }).addTo(map);
 
-$(":button").click(function() {
+var markers = [];
 
 var url_data = $('#text-input1').val();
 
 var downloadData = $.ajax(url_data);
+
+var parsedData = function(data) {
+  var each_entry = JSON.parse(data);
+  var markerCoordinates = each_entry.map(function(entry) {
+  var coords = {
+      lat: entry[ $('#text-input2').val()],
+      long: entry [$('#text-input3').val()]
+
+};
+  return coords;
+});
+  return markerCoordinates;
+};
+
+var makeMarkers = function(parsedData) {
+  markers = parsedData.map(function(coords){
+    return L.marker([coords.lat, coords.long]);
+  });
+  return markers;
+};
+
+var plotMarkers = function(markers) {
+  markers.forEach(function(marker){
+    console.log(marker);
+    return marker.addTo(map);
+  });
+};
+
+var removeMarkers = function(markers) {
+  markers.forEach(function(marker){
+    map.removeLayer(marker);
+  }
+)
+};
+
+/* =====================
+ CODE EXECUTED HERE!
+===================== */
+
+$(":button").click(function() {
+
+if (markers.lenght === 0)
+
+{
+  downloadData.done(function(data) {
+    var parsed = parsedData(data);
+    var markers = makeMarkers(parsed);
+    plotMarkers(markers);
+  });
+} else {
+  removeMarkers(markers);
+  downloadData.done(function(data) {
+    var parsed = parsedData(data);
+    var markers = makeMarkers(parsed);
+    plotMarkers(markers);
+  });
+
+}
+
+});
 
 // COUNTRIES
 // https://raw.githubusercontent.com/MUSA611-CPLN692-spring2020/datasets/master/json/world-country-capitals.json
@@ -38,42 +98,3 @@ var downloadData = $.ajax(url_data);
 // https://raw.githubusercontent.com/MUSA611-CPLN692-spring2020/datasets/master/json/philadelphia-solar-installations.json
 // LAT
 // LONG_
-
-var parsedData = function(data) {
-  var each_entry = JSON.parse(data);
-  var markerCoordinates = each_entry.map(function(entry) {
-  var coords = {
-      lat: entry[ $('#text-input2').val()],
-      long: entry [$('#text-input3').val()]
-
-};
-  return coords;
-});
-  return markerCoordinates;
-};
-
-var makeMarkers = function(parsedData) {
-  var markers = [];
-  markers = parsedData.map(function(coords){
-    return L.marker([coords.lat, coords.long]);
-  });
-  return markers;
-};
-
-var plotMarkers = function(markers) {
-  markers.forEach(function(marker){
-    return marker.addTo(map);
-  });
-};
-
-/* =====================
- CODE EXECUTED HERE!
-===================== */
-
-downloadData.done(function(data) {
-  var parsed = parsedData(data);
-  var markers = makeMarkers(parsed);
-  plotMarkers(markers);
-});
-
-});
